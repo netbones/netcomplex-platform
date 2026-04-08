f964a26ad30cb3e8df8384909664d20f94434c56
David Robert Lewis <ubuntupunk@gmail.com>
2026-04-08 20:53:10 +0200

    fix: add missing Suspense import in Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import { useTranslation } from 'react-i18next';
